3dfd2d63de8b402c47f31c108578e18c27a97199
awangelo <anggg.gaio@gmail.com>
2025-01-09 12:04:41 -0300

    feat: info command with mem usage

--- commands.go
+++ commands.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"runtime"
 	"strings"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -18,6 +21,16 @@ var (
 
 func prepareCommands() {
 	command{
+		Name: "info",
+		Help: "Displays running time and memory usage of the selfbot" + "\n\n" + "Example:" + "\n\n" + "Running for 5m1.793249138s\nUsing: 3 MB",
+		Exec: func(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+			var mem runtime.MemStats
+			runtime.ReadMemStats(&mem)
+			memUsage := fmt.Sprintf("Running for %v\nUsing: %v MB\n", time.Since(loginTime), mem.Alloc/1024/1024)
+			s.ChannelMessageSend(m.ChannelID, memUsage)
+		},
+	}.add()
+	command{
 		Name: "ping",
 		Help: "Responds with Pong!",
 		Exec: func(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
@@ -29,6 +42,9 @@ func prepareCommands() {
 func parseCommand(s *discordgo.Session, m *discordgo.MessageCreate, message string) {
 	msglist := strings.Fields(message)
 	command := msglist[0]
+
+	fmt.Printf("got: %v\n", msglist)
+	fmt.Printf("replied %v to: %v\n", command, m.Author.Username)
 
 	if command == commMap[command].Name {
 		commMap[command].Exec(s, m, msglist[1:])
