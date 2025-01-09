b4122735f2ca855c044d4ef7c8cb781b8df11820
awangelo <anggg.gaio@gmail.com>
2025-01-09 12:20:58 -0300

    feat: create Exec functions for command struct

--- commands.go
+++ commands.go
@@ -23,20 +23,15 @@ func prepareCommands() {
 	command{
 		Name: "info",
 		Help: "Displays running time and memory usage of the selfbot" + "\n\n" + "Example:" + "\n\n" + "Running for 5m1.793249138s\nUsing: 3 MB",
-		Exec: func(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
-			var mem runtime.MemStats
-			runtime.ReadMemStats(&mem)
-			memUsage := fmt.Sprintf("Running for %v\nUsing: %v MB\n", time.Since(loginTime), mem.Alloc/1024/1024)
-			s.ChannelMessageSend(m.ChannelID, memUsage)
-		},
+		Exec: infoCommand,
 	}.add()
-	command{
-		Name: "ping",
-		Help: "Responds with Pong!",
-		Exec: func(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
-			s.ChannelMessageSend(m.ChannelID, "Pong!")
-		},
-	}.add()
+}
+
+func infoCommand(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+	var mem runtime.MemStats
+	runtime.ReadMemStats(&mem)
+	memUsage := fmt.Sprintf("Running for %v\nUsing: %v MB\n", time.Since(loginTime), mem.Alloc/1024/1024)
+	s.ChannelMessageSend(m.ChannelID, memUsage)
 }
 
 func parseCommand(s *discordgo.Session, m *discordgo.MessageCreate, message string) {
