45c693b75279dafb23a774c1308b9529072e7da5
awangelo <anggg.gaio@gmail.com>
2025-01-13 16:16:08 -0300

    feat: create icon command implementation

---
+++ exec/icon.go
@@ -0,0 +1,24 @@
+package exec
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+func Icon(s *discordgo.Session, m *discordgo.MessageCreate, args []string) {
+	if len(args) > 1 {
+		s.ChannelMessageSend(m.ChannelID, "Too many arguments")
+		return
+	}
+
+	var targetUser *discordgo.User
+	if m.ReferencedMessage != nil {
+		targetUser = m.ReferencedMessage.Author
+		icon := targetUser.AvatarURL("2048")
+		s.ChannelMessageSend(m.ChannelID, icon)
+	} else {
+		taget := m.Mentions[0]
+
+		icon := taget.AvatarURL("2048")
+		s.ChannelMessageSend(m.ChannelID, icon)
+	}
+}
